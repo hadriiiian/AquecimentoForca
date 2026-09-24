d3e7301e5412a4522cba32af5b6e323b2b96b7a1
Dri <hadrian.rene@academico.ifpb.edu.br>
2026-09-24 15:45:11 -0300

    questao 1, 2 e resto da 5 | Sexo, Jogador & CentralDeInformacoes

--- src/CentralDeInformacoes.java
+++ src/CentralDeInformacoes.java
@@ -1,24 +1,62 @@
 import java.util.ArrayList;
 public class CentralDeInformacoes {
 	private ArrayList<Jogador> todosJogadores = new ArrayList<Jogador>();
+	private ArrayList<Palavra> palavras = new ArrayList<Palavra>();
 
 	//getters
 	public ArrayList<Jogador> getTodosJogadores() {
 		return todosJogadores;
 	}
-	
+	public ArrayList<Palavra> getPalavras() {
+		return palavras;
+	}
+
 	//setters
 	public void setTodosJogadores(ArrayList<Jogador> todosJogadores) {
 		this.todosJogadores = todosJogadores;
 	}
 	
-	public boolean add(Jogador j) {
-		if(j == null)
-			return false;
+	//em relação à todosJogadores
+	public boolean addJogador(Jogador j) {
+		for (int i = 0; i < todosJogadores.size(); i++) {
+			if (todosJogadores.get(i).equals(j))
+				return false;
+		}
 		todosJogadores.add(j);
 		return true;
 	}
 	
 	public Jogador readCPF(String cpf) {
+		for (int i = 0; i < todosJogadores.size(); i++) {
+			if (todosJogadores.get(i).getCPF().equals(cpf))
+				return todosJogadores.get(i);
+		}
+		return null;
+	}
+	
+	public Jogador readEmail(String email) {
+		for (int i = 0; i < todosJogadores.size(); i++) {
+			if (todosJogadores.get(i).getEmail().equals(email))
+				return todosJogadores.get(i);
+		}
+		return null;
+	}
+	
+	//em relação à palavras
+	public boolean addPalavra(Palavra p) {
+		for (int i = 0; i < palavras.size(); i++) {
+			if (palavras.get(i).equals(p))
+				return false;
+		}
+		palavras.add(p);
+		return true;
+	}
+	
+	public Palavra readPalavra(String palavra) {
+		for (int i = 0; i < palavras.size(); i++) {
+			if (palavras.get(i).getPalavra().equals(palavra))
+				return palavras.get(i);
+		}
+		return null;
 	}
 }
--- src/Jogador.java
+++ src/Jogador.java
@@ -1,3 +1,53 @@
-public class Jogador {
 
+public class Jogador {
+	private String nome;
+	private String email;
+	private Sexo sexo;
+	private String cpf;
+
+	//getters
+	public String getNome() {
+		return nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public Sexo getSexo() {
+		return sexo;
+	}
+	public String getCPF() {
+		return cpf;
+	}
+	
+	//setters
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+
+	//construtor
+	public Jogador(String nome, String email, Sexo sexo, String cpf) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.sexo = sexo;
+		this.cpf = cpf;
+	}
+	
+	public String toString() {
+		return nome;
+	}
+	
+	public boolean equals(Object obj) {
+		if (this == obj) 
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
+		
+		Jogador j = (Jogador) obj;
+		
+		return (cpf.equals(j.getCPF()) && email.equals(j.getEmail()));
+	}
 }
