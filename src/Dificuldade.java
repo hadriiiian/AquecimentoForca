300e0324880419452cb247406a20fa42d6cde957
João Augusto <joao.014araujo@gmail.com>
2026-09-24 17:29:23 -0300

    Criei classe Palavra com atributos, construtor, gets/sets, equals e toString; criei enum Dificuldade | Palavra.java, Dificuldade.java

---
+++ src/Dificuldade.java
@@ -0,0 +1,4 @@
+
+public enum Dificuldade {
+    FACIL, MEDIO, DIFICIL
+}
--- src/Palavra.java
+++ src/Palavra.java
@@ -1,14 +1,15 @@
+import java.time.LocalDate;
 public class Palavra {
 	private String palavra;
 	private String dica;
-	
-	//construtor
-	public Palavra() {
-	}
-	
-	public Palavra(String palavra, String dica) {
+	private Dificuldade nivelDeDificuldade;
+	private LocalDate dataCadastro;
+
+
+	public Palavra(String palavra, String dica, Dificuldade nivelDeDificuldade) {
 		this.palavra = palavra;
 		this.dica = dica;
+		this.nivelDeDificuldade = nivelDeDificuldade;
 	}
 
 	//getters
@@ -18,6 +19,12 @@ public class Palavra {
 	public String getDica() {
 		return dica;
 	}
+	public Dificuldade getNivelDeDificuldade() {
+		return nivelDeDificuldade;
+	}
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
 
 	//setters
 	public void setPalavra(String palavra) {
@@ -26,5 +33,21 @@ public class Palavra {
 	public void setDica(String dica) {
 		this.dica = dica;
 	}
+	public void setNivelDeDificuldade(Dificuldade nivelDeDificuldade) {
+		this.nivelDeDificuldade = nivelDeDificuldade;
+	}
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	
+	public boolean equals(Object obj) {
+		Palavra outra = (Palavra) obj;
+		return this.palavra.equals(outra.palavra);
+	}
+	
+	public String toString() {
+		return "Palavra: " + palavra + "|" + "Dica: " + dica;
+	}
+
 	
 }
