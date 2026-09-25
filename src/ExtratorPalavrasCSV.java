91b863f5fe7197182d22005181f9276102c435c0
eduardohdev <eduardohipolito2012@gmail.com>
2026-09-25 17:00:02 -0300

    Concluído classe ExtratorDePalavrasCSV

--- src/ExtratorPalavrasCSV.java
+++ src/ExtratorPalavrasCSV.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File; // representa caminho de arquivo e verifica sua existência
+
+import java.nio.charset.StandardCharsets; // padrão de codificação (UTF-8) para correção de acentos
+
+
 
 public class ExtratorPalavrasCSV {
-
+	public static ArrayList<Palavra> extrairPalavras(String nomeArquivoCSV){
+		ArrayList<Palavra> palavras = new ArrayList<>();
+		File arquivo = new File(nomeArquivoCSV);
+		
+		if(!arquivo.exists()) {
+			return null;
+		}
+		
+		try (Scanner ler = new Scanner(arquivo, StandardCharsets.UTF_8.name())) {
+			while (ler.hasNextLine()) {
+				String linha = ler.nextLine().trim();
+				
+				if (linha.isEmpty()) {continue;}		
+			
+			String[] partes = linha.split(",");
+			
+			if (partes.length < 3) {
+				return null;
+			}
+			
+			String termo = partes[0].trim();
+			int dificuldade = Integer.parseInt(partes[1].trim());
+			Dificuldade dificuldadeEnum = Dificuldade.values()[dificuldade];
+			String dica = partes[2].trim();
+			
+			Palavra p = new Palavra(termo, dica, dificuldadeEnum);
+			palavras.add(p);
+		}
+	} catch(Exception e) {
+		return null;
+	}
+	
+	return palavras;
+}
 }
